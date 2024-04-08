a154bd2785fca31b792fcd52cb7afb25ae28e674
Max Ramon <MaxRamonNB@Gmail.com>
2024-04-08 13:53:32 -0300

    update

---
+++ UI Advanced/Assets/_Scrips/ButtonConfiguration.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using DG.Tweening;
+
+
+
+
+public class ButtonConfiguration: MonoBehaviour
+{
+    public List<GameObject> myObjects;
+    public float delayBetweenObjects = .1f;
+    public float animationDuration = .3f;
+
+
+    [Header("Animation")]
+
+    public Animator animator;
+    public string triggerShow = "Show";
+    public string triggerHide = "Hide";
+    private bool _IsShowing = false;
+
+    private void Awake()
+    {
+        //GetObjects();
+        Hide();
+    }
+    private void GetObjects()
+    {
+        foreach (Transform t in GetComponentInChildren<Transform>())
+        {
+            if (t != transform)
+                myObjects.Add(t.gameObject);
+
+        }
+    }
+
+    private void Hide()
+    {
+        _IsShowing = false;
+        foreach (GameObject g in myObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+    public void ShowItens()
+    {
+        if (_IsShowing)
+        {
+            Hide();
+            animator.SetTrigger(triggerHide);
+        }
+        else
+        {
+            animator.SetTrigger(triggerShow);
+            _IsShowing = true;
+            StartCoroutine(Show());
+
+
+        }
+    }
+    IEnumerator Show()
+    {
+        foreach (GameObject g in myObjects)
+        {
+            yield return new WaitForSeconds(delayBetweenObjects);
+            g.SetActive(true);
+            g.transform.DOScale(0, animationDuration).From();
+        }
+    }
+
+}
